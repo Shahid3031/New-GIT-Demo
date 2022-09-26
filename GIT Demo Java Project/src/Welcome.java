5ff45c77b85e9640a60fff2747504ace9bdbe4fa
Administrator <shahidafridishaik2911@gmail.com>
2022-09-26 16:54:42 +0530

    New Commit

--- GIT Demo Java Project/src/Welcome.java
+++ GIT Demo Java Project/src/Welcome.java
@@ -3,11 +3,12 @@ public class Welcome {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome to GIT Demo");
-		System.out.println("Hi Hello");
-		System.out.println("Hi Der");
 		System.out.println("Hi World");
-		System.out.println("Hello World");
+		System.out.println("Hi Hello");
+		System.out.println("Hi There");
+		System.out.println("Hi Im Shahid Afridi");
+		System.out.println("Hi How are you");
+		
 
 	}
 
